66211c5a324d4585d5925daaf22359222065fd6c
rishkatt <rishkatt@gmail.com>
2025-12-24 13:44:56 -0500

    finish route

--- exit.py
+++ exit.py
@@ -7,7 +7,7 @@ def title_screen():
     clear()
     print("eXit\n")
     print("Press ENTER to begin")
-    input()  # Waits for ENTER
+    input()
 
 def first_room():
     clear()
@@ -16,8 +16,8 @@ def first_room():
     print("What do you do?\n")
 
     while True:
-        choice = input("> ").strip()
-        if choice.lower() == "move the barrel":
+        choice = input("> ").strip().lower()
+        if choice == "move the barrel":
             return
         else:
             print("That doesn't work.")
@@ -27,10 +27,82 @@ def second_room():
     print("The barrel rolls aside and you find a secret tunnel.")
     print("What do you do?\n")
 
+    while True:
+        choice = input("> ").strip().lower()
+        if choice == "enter the tunnel":
+            return
+        else:
+            print("That doesn't work.")
+
+def third_room():
+    clear()
+    print("You start to escape but your friend is too weak to go with you.")
+    print("They hand you a note.")
+    print("What do you do?\n")
+
+    while True:
+        choice = input("> ").strip().lower()
+        if choice == "read note":
+            break
+        else:
+            print("That doesn't work.")
+
+    clear()
+    print("It is too dark to read the note.")
+    print("What do you do?\n")
+
+    while True:
+        choice = input("> ").strip().lower()
+        if choice == "leave":
+            break
+        else:
+            print("That doesn't work.")
+
+    clear()
+    print("You crawl through the tunnel and the tunnel leads you to a beach.")
+    print("What do you do?\n")
+
+    while True:
+        choice = input("> ").strip().lower()
+        if choice == "look":
+            break
+        else:
+            print("That doesn't work.")
+
+    clear()
+    print("In the water, you see a boat.")
+    print("What do you do?\n")
+
+    while True:
+        choice = input("> ").strip().lower()
+        if choice == "get on the boat":
+            break
+        else:
+            print("That doesn't work.")
+
+def ending():
+    clear()
+    print("Congratulations, you're heading to a new world!")
+    print("Do you want to play again? (Y/N)\n")
+
+    while True:
+        choice = input("> ").strip().lower()
+        if choice == "y":
+            return True
+        elif choice == "n":
+            return False
+        else:
+            print("Please enter Y or N.")
+
 def main():
-    title_screen()
-    first_room()
-    second_room()
+    while True:
+        title_screen()
+        first_room()
+        second_room()
+        third_room()
+        if not ending():
+            clear()
+            break
 
 if __name__ == "__main__":
     main()
